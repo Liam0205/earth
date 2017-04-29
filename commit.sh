dcbc0a0ef433f26dee56a99e22affc776833cb76
Liam Huang <liamhuang0205@gmail.com>
2017-04-29 23:45:23 +0800

    start

---
+++ commit.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+begin=$(expr $(cat ./begin) + 86400) # init: 972576000
+end=$(date '+%s')
+
+list=$(seq $begin 86400 $end)
+after=''
+
+for i in $list
+do
+    date -u --date=@$i '+%Y年%m月%d日：没有' >> ./README.md
+    echo >> ./README.md
+
+    echo $i > ./begin
+
+    git add ./README.md
+    git add ./begin
+    git commit --date=$i --message="${end}"
+
+    after='git push origin HEAD'
+done
+
+eval $after
+
